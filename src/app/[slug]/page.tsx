ba5c62e556215aac3649599f67ed60b041063154
Zane St. John <zstjohn@stanford.edu>
2023-09-05 16:20:44 -0700

    social links & spotify preview

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,11 +1,27 @@
 import type { Metadata } from "next";
-import { Button, Flex, Heading } from "@/app/components/chakra";
+import { Box, Button, Flex, Heading, Link } from "@/app/components/chakra";
 import AutoStyledContent from "@/app/components/shared/AutoStyledContent";
 import { type ButtonProps } from "@chakra-ui/react";
+import type { IconType } from "react-icons";
 import PosterImage from "../components/shared/PosterImage";
 
-import GROUPS from "@/app/groups";
+import GROUPS, {
+  type ACappellaGroup,
+  type GroupSocialLinks,
+} from "@/app/groups";
 import { notFound } from "next/navigation";
+
+// icons
+import {
+  SiInstagram,
+  SiYoutube,
+  SiSpotify,
+  SiApplemusic,
+  SiTiktok,
+  SiTwitter,
+  SiFacebook,
+} from "react-icons/si";
+import { useMemo } from "react";
 
 export function generateMetadata({
   params: { slug },
@@ -46,16 +62,19 @@ export default function GroupPage({
 
   return (
     <>
-      <Heading size="xl" mb="4">
+      <Heading as="h2" size="xl" mb="4">
         {group.name}
       </Heading>
-      <Flex direction={{ base: "column", md: "row" }} gap="6">
+      <Flex direction={{ base: "column", md: "row" }} gap="8">
         <Flex
           direction="column"
           gap="4"
           w={{ base: "auto", md: "270px", mdPlus: "315px", lg: "345px" }}
         >
           <PosterImage group={group} />
+          {group.socialLinks && Object.keys(group.socialLinks).length > 0 && (
+            <SocialLinks group={group} />
+          )}
           <Flex direction="column" w="100%" gap="1">
             {group.auditionLink && (
               <Button colorScheme="red" {...linkButton(group.auditionLink)}>
@@ -67,11 +86,95 @@ export default function GroupPage({
             </Button>
           </Flex>
         </Flex>
-        <AutoStyledContent lineHeight="1.3em" flex={{ base: undefined, md: 1 }}>
-          {group.description}
-        </AutoStyledContent>
+        <Box flex={{ base: undefined, md: 1 }}>
+          <AutoStyledContent lineHeight="1.3em">
+            {group.description}
+          </AutoStyledContent>
+          {/* Spotify Preview (for groups with listed Spotify) */}
+          {group.socialLinks?.spotify && (
+            <>
+              <Heading size="md" as="h3" my="2">
+                Listen
+              </Heading>
+              <SpotifyArtistEmbed spotifyUrl={group.socialLinks.spotify} />
+            </>
+          )}
+        </Box>
       </Flex>
     </>
+  );
+}
+
+function SpotifyArtistEmbed({ spotifyUrl }: { spotifyUrl: string }) {
+  const pathname = useMemo(() => new URL(spotifyUrl).pathname, [spotifyUrl]);
+
+  return (
+    <iframe
+      style={{ borderRadius: "12px" }}
+      src={"https://open.spotify.com/embed" + pathname}
+      width="100%"
+      height="152"
+      frameBorder={0}
+      allowFullScreen
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+  );
+}
+
+const SOCIAL_LINK_NAMES: {
+  [k in keyof GroupSocialLinks]: string;
+} = {
+  instagram: "Instagram",
+  youtube: "YouTube",
+  spotify: "Spotify",
+  appleMusic: "Apple Music",
+  tiktok: "TikTok",
+  twitter: "Twitter",
+  facebook: "Facebook",
+};
+
+const SOCIAL_LINK_ICONS: {
+  [k in keyof GroupSocialLinks]: IconType;
+} = {
+  instagram: SiInstagram,
+  youtube: SiYoutube,
+  spotify: SiSpotify,
+  appleMusic: SiApplemusic,
+  tiktok: SiTiktok,
+  twitter: SiTwitter,
+  facebook: SiFacebook,
+};
+
+function SocialLinks({
+  group: { name, socialLinks },
+}: {
+  group: ACappellaGroup;
+}) {
+  return (
+    <Flex alignItems="center" justifyContent="center" gap="3">
+      {Object.entries(SOCIAL_LINK_ICONS)
+        .filter(([key]) => !!socialLinks![key as keyof GroupSocialLinks])
+        .map(([key, IconComponent]) => (
+          <Link
+            key={key}
+            href={socialLinks![key as keyof GroupSocialLinks]!}
+            target="_blank"
+            rel="noopener noreferrer"
+            title={
+              name + " " + SOCIAL_LINK_NAMES[key as keyof GroupSocialLinks]
+            }
+            color="#821b"
+            transition="color 200ms ease"
+            sx={{
+              "& svg": { fontSize: "1.75rem" },
+              "&:hover": { color: "#821e" },
+            }}
+          >
+            <IconComponent />
+          </Link>
+        ))}
+    </Flex>
   );
 }
 
--- src/app/groups.tsx
+++ src/app/groups.tsx
@@ -10,6 +10,16 @@ import Testimony from "./components/group-pages/Testimony";
 import Talisman from "./components/group-pages/Talisman";
 import MixedCompany from "./components/group-pages/MixedCompany";
 
+export interface GroupSocialLinks {
+  instagram: string;
+  youtube: string;
+  spotify: string;
+  appleMusic: string;
+  tiktok: string;
+  twitter: string;
+  facebook: string;
+}
+
 export interface ACappellaGroup {
   name: string;
   tagline: string;
@@ -20,6 +30,8 @@ export interface ACappellaGroup {
   auditionLink?: string;
   // Optional: extra keywords for SEO
   extraKeywords?: string[];
+  // Social media links
+  socialLinks?: Partial<GroupSocialLinks>;
 }
 
 const GROUPS: { [slug: string]: ACappellaGroup } = {
@@ -30,6 +42,14 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     imgUrl: "/assets/img/fleet-street.jpg",
     siteLink: "https://www.fleetstreet.com/",
     auditionLink: "https://www.fleetstreet.com/audition",
+    socialLinks: {
+      youtube: "https://www.youtube.com/user/FleetStreetSingers",
+      appleMusic:
+        "https://music.apple.com/us/artist/stanford-fleet-street-singers/4165870",
+      instagram: "https://www.instagram.com/thefleetstagram/",
+      spotify: "https://open.spotify.com/artist/56MfXqkYeRrlMzLhBMg9CG",
+      facebook: "https://www.facebook.com/StanfordFleetStreetSingers/",
+    },
   },
   mendicants: {
     name: "Mendicants",
@@ -37,6 +57,15 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     description: <Mendicants />,
     imgUrl: "/assets/img/mendicants.jpg",
     siteLink: "https://www.stanfordmendicants.com/",
+    socialLinks: {
+      spotify: "https://open.spotify.com/artist/3ZBvLOZu5A9jtKO5XjHDTl",
+      appleMusic:
+        "https://music.apple.com/us/artist/the-stanford-mendicants/845314849",
+      instagram: "https://www.instagram.com/the_mendicants/",
+      tiktok: "https://www.tiktok.com/@the_mendicants",
+      youtube: "https://www.youtube.com/@StanfordMendicants",
+      facebook: "https://www.facebook.com/mendicants/",
+    },
   },
   counterpoint: {
     name: "Counterpoint",
@@ -44,6 +73,13 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     description: <Counterpoint />,
     imgUrl: "/assets/img/counterpoint.jpg",
     siteLink: "https://counterpointacappella.com/",
+    socialLinks: {
+      youtube: "https://www.youtube.com/user/StanfordCPT",
+      instagram: "https://www.instagram.com/stanfordcounterpoint/",
+      spotify: "https://open.spotify.com/artist/4nZ5WW3me2FaE1IIEvE6bB",
+      twitter: "https://twitter.com/su_counterpoint",
+      facebook: "https://www.facebook.com/StanfordCounterpoint",
+    },
   },
   harmonics: {
     name: "Harmonics",
@@ -52,6 +88,12 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     imgUrl: "/assets/img/harmonics.jpg",
     siteLink: "https://www.stanfordharmonics.com/",
     extraKeywords: ["harmz", "stanford harmz"],
+    socialLinks: {
+      spotify: "https://play.spotify.com/artist/528Rcthd9JqtIrlbrGKNsG",
+      instagram: "http://instagram.com/stanfordharmonics/",
+      youtube: "https://www.youtube.com/user/StanfordHarmonics",
+      facebook: "https://www.facebook.com/theharmonics",
+    },
   },
   raagapella: {
     name: "Raagapella",
@@ -59,6 +101,15 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     description: <Raagapella />,
     imgUrl: "/assets/img/raagapella.jpg",
     siteLink: "https://www.raagapella.com/",
+    socialLinks: {
+      instagram: "https://www.instagram.com/stanfordraagapella/",
+      tiktok: "https://www.tiktok.com/@stanfordraagapella",
+      youtube: "https://www.youtube.com/c/StanfordRaagapella",
+      spotify: "https://open.spotify.com/artist/7biaLUJ0zBroX0Wj4hfiU9",
+      facebook: "https://www.facebook.com/StanfordRaagapella/",
+      appleMusic:
+        "https://music.apple.com/us/artist/stanford-raagapella/1104162044",
+    },
   },
   "o-tone": {
     name: "O-Tone",
@@ -67,6 +118,12 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     imgUrl: "/assets/img/o-tone.jpg",
     siteLink: "https://stanfordotone.com/",
     extraKeywords: ["otone", "stanford otone"],
+    socialLinks: {
+      youtube: "https://www.youtube.com/channel/UCalFurLezFrabetPytEvVFQ",
+      instagram: "https://www.instagram.com/stanfordotone",
+      facebook: "https://www.facebook.com/StanfordOTone",
+      spotify: "https://open.spotify.com/artist/6AHkKb8FrqJ0ZgxL40eoHk",
+    },
   },
   "everyday-people": {
     name: "Everyday People",
@@ -75,6 +132,12 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     imgUrl: "/assets/img/everyday-people.jpg",
     siteLink: "https://linktr.ee/stanfordeverydaypeople",
     extraKeywords: ["ep", "stanford ep"],
+    socialLinks: {
+      instagram: "https://www.instagram.com/stanford_everydaypeople/",
+      youtube: "https://www.youtube.com/user/StanfoEverydayPeople",
+      twitter: "https://twitter.com/epacappella",
+      facebook: "https://www.facebook.com/StanfordEverydayPeople/",
+    },
   },
   testimony: {
     name: "Testimony",
@@ -82,6 +145,12 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     description: <Testimony />,
     imgUrl: "/assets/img/testimony.jpg",
     siteLink: "https://testimonyacappella.weebly.com/",
+    socialLinks: {
+      youtube: "https://www.youtube.com/user/testimonyacappella",
+      instagram: "https://www.instagram.com/testimonyacappella/",
+      facebook: "https://www.facebook.com/testimonyacappella/",
+      spotify: "https://open.spotify.com/artist/1Io1lNYlEkOHVzmSoDhBzm",
+    },
   },
   talisman: {
     name: "Talisman",
@@ -89,6 +158,13 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     description: <Talisman />,
     imgUrl: "/assets/img/talisman.jpg",
     siteLink: "http://www.stanfordtalisman.com/",
+    socialLinks: {
+      youtube: "https://www.youtube.com/user/stanfordtalisman",
+      facebook: "https://www.facebook.com/pages/Stanford-Talisman/6343264665",
+      instagram: "http://instagram.com/stanfordtalisman/",
+      twitter: "https://twitter.com/su_talisman",
+      spotify: "https://open.spotify.com/artist/3UIMILG35CbsLIZU36eBWu",
+    },
   },
   "mixed-company": {
     name: "Mixed Company",
@@ -98,6 +174,15 @@ const GROUPS: { [slug: string]: ACappellaGroup } = {
     imgUrl: "/assets/img/mixed-company.jpg",
     siteLink: "https://www.mixedco.com/",
     extraKeywords: ["mixed co", "mixedco", "mixed-co", "stanford mixed co"],
+    socialLinks: {
+      youtube: "https://www.youtube.com/channel/UCbweG2JD_9iO50jWnQQnwAQ",
+      facebook: "https://www.facebook.com/mixedco/",
+      twitter: "https://twitter.com/mixedcompany",
+      instagram: "https://www.instagram.com/stanfordmixedco/",
+      appleMusic:
+        "https://music.apple.com/us/artist/stanford-mixed-company/4165885",
+      spotify: "https://open.spotify.com/artist/2W6zg3XdtRCYc7DStZTFAq",
+    },
   },
 };
 
