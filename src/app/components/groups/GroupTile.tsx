ffe8229632ada2835d9ea10d6fc1ceff861df064
Zane St. John <zstjohn@stanford.edu>
2023-09-05 16:56:17 -0700

    ux

--- src/app/components/groups/GroupTile.tsx
+++ src/app/components/groups/GroupTile.tsx
@@ -18,6 +18,8 @@ export default function GroupTile({
       href={"/" + slug}
       textAlign="center"
       sx={{
+        // Remove ugly gray tap box on mobile Safari (just for these components)
+        WebkitTapHighlightColor: "transparent",
         "& .posterImage": {
           transform: "scale(1)",
           transition: "transform 200ms ease",
