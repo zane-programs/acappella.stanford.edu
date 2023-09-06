7e6c8617798fe49849020582747d4c19324aa67e
Zane St. John <zstjohn@stanford.edu>
2023-09-06 00:11:26 -0700

    gtag event on share

--- src/app/components/shared/ShareButton.tsx
+++ src/app/components/shared/ShareButton.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useCallback } from "react";
+
 import { MdShare } from "react-icons/md";
 import { Button } from "@/app/components/chakra";
 
@@ -7,25 +9,32 @@ const hasShareSupport =
   typeof window !== "undefined" && "share" in window.navigator;
 
 export default function ShareButton() {
+  const handleShare = useCallback(() => {
+    navigator
+      .share({
+        title: document.title,
+        text:
+          document
+            .querySelector(`meta[name="description"]`)
+            ?.getAttribute("content") ??
+          "StanfordACappella.com - The Digital Home of A Cappella at Stanford University",
+        url: window.location.origin + window.location.pathname,
+      })
+      .then(() => {
+        console.log("Shared!");
+
+        // Log analytics event
+        typeof window !== "undefined" &&
+          window?.gtag("event", "shareWithShareButton", {
+            event_category: "groupPage",
+            event_label: document.title,
+          });
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
   return hasShareSupport ? (
-    <Button
-      colorScheme="green"
-      leftIcon={<MdShare />}
-      onClick={() =>
-        navigator
-          .share({
-            title: document.title,
-            text:
-              document
-                .querySelector(`meta[name="description"]`)
-                ?.getAttribute("content") ??
-              "StanfordACappella.com - The Digital Home of A Cappella at Stanford University",
-            url: window.location.origin + window.location.pathname,
-          })
-          .then(() => console.log("Success"))
-          .catch((e) => console.error(e))
-      }
-    >
+    <Button colorScheme="green" leftIcon={<MdShare />} onClick={handleShare}>
       Share
     </Button>
   ) : null;
