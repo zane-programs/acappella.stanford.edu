67e230a7a00b5db8660af0072be3c3b8aec4705f
Zane St. John <zstjohn@stanford.edu>
2023-09-20 00:52:22 -0700

    fixed co

--- src/app/components/group-pages/MixedCompany.tsx
+++ src/app/components/group-pages/MixedCompany.tsx
@@ -4,20 +4,21 @@ export default function MixedCompany() {
   return (
     <>
       <Text>
-        Two-time ICCA Semifinalists, and featured on several Best of College A
-        Cappella and Voices Only compilation CDs, Stanford Mixed
-        Company&mdash;or Mixed Co&mdash;has been widely recognized for its
-        talent and professionalism, both on campus and around the country for
-        almost 40 years. As the group&apos;s name suggests, Mixed Co produces a
-        wide variety of music from all genres, singing today&apos;s top hits,
-        classics from previous decades, exciting songs by up-and-coming artists,
-        and throwbacks from yesteryear.
+        2022 and 2023 ICCA semifinalists, and featured on several Best of
+        College A Cappella and Voices Only compilation CDs, Stanford Mixed
+        Company—or Mixed Co—is Stanford&apos;s oldest all-gender acapella group,
+        founded in 1985. Widely recognized both on campus and around the
+        country, Mixed Co produces a wide variety of music from all genres,
+        singing pop, R&B, indie hits, classics, and more. Every year, they put
+        on two major concerts: an anti-Valentine&apos;s Day show called Love
+        Sucks in February, and their Spring Show in May, as well as performing
+        in gigs and shows around campus and the Bay Area.
       </Text>
       <Text>
         Mixed Co strives to build a meaningful community by fostering strong
         bonds between current members and continuing the traditions of the
         group&apos;s more than 250 alumni. The group boasts 16 albums to date,
-        with more music on the way soon!
+        and is working to release their next album soon!
       </Text>
     </>
   );
