0ced8843f41638bb570e149f747f9d29984226c0
Zane St. John <zstjohn@stanford.edu>
2023-09-23 13:32:52 -0700

    harmz update today

--- src/app/components/group-pages/Harmonics.tsx
+++ src/app/components/group-pages/Harmonics.tsx
@@ -8,8 +8,17 @@ export default function Harmonics() {
         for their unique sound and award-winning recordings, the Harmonics have
         garnered international recognition for their performances and have been
         featured on BOCA, Sing, and Voices Only a cappella compilations. Signal
-        Lost, the Harmonics&apos; most recent album, won the CARA award for Best Rock
-        Album in 2020.
+        Lost, the Harmonics&apos; most recent album, won the CARA award for Best
+        Rock Album in 2020.
+      </Text>
+      <Text>
+        Our alumni network is a tight-knit community; at our 30 year reunion
+        last year, we had members from every single year of the group&apos;s
+        existence in attendance. We save the front row of each of our shows for
+        alums attending from near and far and invite them up to sing an
+        arrangement that has been performed in Harmz since the group&apos;s
+        inception in 1991. We strive to create a caring, supportive, and
+        inclusive environment that stretches far beyond our love of music.
       </Text>
     </>
   );
