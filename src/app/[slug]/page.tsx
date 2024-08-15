e485ad01582612a697afb1a6fd86b967cf333cf4
Zane St. John <zstjohn@stanford.edu>
2024-08-16 01:07:07 +0300

    last time

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -215,11 +215,11 @@ function SocialLinks({
             href={socialLinks![key as keyof GroupSocialLinks]!}
             target="_blank"
             rel="noopener noreferrer"
-            color="#bd2828"
+            color="#8c1515"
             transition="color 200ms ease"
             sx={{
               "& svg": { fontSize: "1.75rem" },
-              "&:hover": { color: "#8c1515" },
+              "&:hover": { color: "#bd2828" },
             }}
             title={
               name + " " + SOCIAL_LINK_NAMES[key as keyof GroupSocialLinks]
