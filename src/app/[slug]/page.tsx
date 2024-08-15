2bc598c6d910ecf08c5e5ed668ceec66be73961e
Zane St. John <zstjohn@stanford.edu>
2024-08-16 01:04:06 +0300

    fix social color on hover

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -215,11 +215,11 @@ function SocialLinks({
             href={socialLinks![key as keyof GroupSocialLinks]!}
             target="_blank"
             rel="noopener noreferrer"
-            color="#8c1515"
+            color="#821b"
             transition="color 200ms ease"
             sx={{
               "& svg": { fontSize: "1.75rem" },
-              "&:hover": { color: "#821e" },
+              "&:hover": { color: "#bd2828" },
             }}
             title={
               name + " " + SOCIAL_LINK_NAMES[key as keyof GroupSocialLinks]
